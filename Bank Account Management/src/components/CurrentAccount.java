3cdebe7fcff521c0bf3b77af8208fef064445ae3
nyl finez <nylcalvario@gmail.com>
2023-12-05 11:31:40 +0100

    Added classes: Current and Savings

---
+++ Bank Account Management/src/components/CurrentAccount.java
@@ -0,0 +1,9 @@
+package components;
+
+public class CurrentAccount extends Account {
+
+	public CurrentAccount(String label, Client client) {
+		super(label, client);
+	}
+
+}
---
+++ Bank Account Management/src/components/SavingsAccount.java
@@ -0,0 +1,9 @@
+package components;
+
+public class SavingsAccount extends Account {
+
+	public SavingsAccount(String label, Client client) {
+		super(label, client);
+	}
+
+}
