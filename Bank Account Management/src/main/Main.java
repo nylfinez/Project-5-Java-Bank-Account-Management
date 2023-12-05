b3c638155331b7d90190bf86fbe383b65c6b241e
nyl finez <nylcalvario@gmail.com>
2023-12-05 12:23:00 +0100

    Added load and display accounts to Main class

--- Bank Account Management/src/main/Main.java
+++ Bank Account Management/src/main/Main.java
@@ -1,18 +1,26 @@
 package main;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+
+import components.Account;
 import components.Client;
+import components.CurrentAccount;
+import components.SavingsAccount;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Client[] clients = makeClientArray(4);
-		displayClient(clients);
+		Client[] clients = loadClients(4);
+		displayClients(clients);
 
+		List<Account> accounts = loadAccounts(clients);
+		displayAccounts(accounts);
 	}
 	
 	//generate clients
-		private static Client[] makeClientArray(int numberOfClients) {
+		private static Client[] loadClients(int numberOfClients) {
 			Client[] clients = new Client[numberOfClients];
 			for (int i = 0; i < numberOfClients; i++) {
 				clients[i] = new Client("name"+(i+1), "firstName"+(i+1));
@@ -21,10 +29,31 @@ public class Main {
 		}
 		
 		//display clients
-		private static void displayClient(Client[] clients) {
+		private static void displayClients(Client[] clients) {
 			Arrays.stream(clients)
 			.map(Client::toString)
 			.forEach(System.out::println);
 		}
 
+		//generate accounts
+		private static List<Account> loadAccounts(Client[] clients) {
+			List<Account> accounts = new ArrayList<>();
+			
+			for (Client client : clients) {
+				CurrentAccount currentAccount = new CurrentAccount("Current", client);
+				SavingsAccount savingsAccount = new SavingsAccount("Savings", client);
+				
+				accounts.add(currentAccount);
+				accounts.add(savingsAccount);
+			}
+			
+			return accounts;
+		}
+		
+		//display accounts
+		private static void displayAccounts(List<Account> accounts) {
+			accounts.stream()
+			.map(Account::toString)
+			.forEach(System.out::println);
+		}
 }
