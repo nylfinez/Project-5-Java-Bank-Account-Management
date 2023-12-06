cb7a998b98a89c1c8eb0a8b1a22a23f799e91f24
nyl finez <nylcalvario@gmail.com>
2023-12-06 17:31:50 +0100

    Removed unnecessary import

--- Bank Account Management/src/main/Main.java
+++ Bank Account Management/src/main/Main.java
@@ -10,7 +10,6 @@ import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import components.Account;
 import components.Client;
